f525457566c394a2bc998fa6c8d6c2a9ac9f405a
diegobtt <diego.bitencourt@gran.com>
2024-11-01 11:41:58 -0300

    done

--- public/index.php
+++ public/index.php
@@ -11,23 +11,23 @@ $app = AppFactory::create();
 
 require '../app/routes/api.php';
 
-// Exemplo de middleware para CORS
+
 $app->add(function ($request, $handler) {
-    // Permitir todos os domínios e métodos HTTP
+ 
     $response = new \Slim\Psr7\Response();
     $response = $response->withHeader('Access-Control-Allow-Origin', '*')
                          ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                          ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    // Verifique se a requisição é do tipo OPTIONS
+  
     if ($request->getMethod() === 'OPTIONS') {
         return $response;
     }
 
-    // Continue o processamento da requisição
+   
     $response = $handler->handle($request);
 
-    // Retorne a resposta com os cabeçalhos de CORS
+   
     return $response->withHeader('Access-Control-Allow-Origin', '*')
                     ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
                     ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
